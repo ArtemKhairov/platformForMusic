4b6ac4899ace7f0f48238c45735243da21d85f71
ArtemKhairov <artemxairoff@yandex.ru>
2021-04-11 00:48:22 +0700

    frontend
    Create app with Next.js
    add client side
    add MainLayout
    add tracks page

---
+++ client/pages/index.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import MainLayout from "../layout/Mainlayout";
+
+export default function index() {
+  return (
+    <>
+      <MainLayout>
+        <div className="center">
+          <h1>Добро пожаловать!</h1>
+          <h3>Лучший схрон треков</h3>
+        </div>
+      </MainLayout>
+
+      <style jsx>
+        {`
+          .center {
+            margin-top: 100px;
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+            justify-content: center;
+          }
+        `}
+      </style>
+    </>
+  );
+}
